288dc6e8cc5a6b5c16d7516c125d016cee71b06a
sachinhp7 <sachinhp725083@gmail.com>
2024-03-27 15:06:46 +0530

    create welcome page with top black container bar

--- final/my-react-app/src/WelcomePage.js
+++ final/my-react-app/src/WelcomePage.js
@@ -1,13 +1,25 @@
-
+import React from 'react';
+import './App.css';
+import mylogo from './logo.png';
 
 const WelcomePage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page content.</p>
+    <div className="App">
+      {/* Content box with black background */}
+      <div className="content-box black-background">
+        {/* Logo on the left */}
+        <img src={mylogo} className="logo" alt="logo" />
+        <div className="content">
+          {/* Navigation bar on the right */}
+          <nav className="navigation">
+            <button className="nav-button">Home</button>
+            <button className="nav-button">About</button>
+            <button className="nav-button">Contact</button>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default WelcomePage;
-
+export default  WelcomePage;
