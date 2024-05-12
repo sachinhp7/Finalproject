c480b3340325ae35e3d3837815d25c6a9303e02e
sachinhp7 <88194255+sachinhp7@users.noreply.github.com>
2024-05-12 06:54:31 +0530

    added database file and connection file

---
+++ database/data.js
@@ -0,0 +1,166 @@
+export default [
+    {
+        id: 1,
+        question : "Which of the following sets is finite?",
+        options : [
+            'The set of all even numbers.',
+            'The set of all multiples of 5.',
+            'The set of all planets in our solar system.',
+            'The set of all prime numbers.',
+        ]
+    },
+    {
+        id: 2,
+        question : "Which of the following sets is infinite?",
+        options : [
+            'The set of digits in your phone number',
+            'The set of months in a year.',
+            'The set of all integers less than 10',
+            'The set of all possible scores when rolling a die.',
+        ]
+    },
+    {
+        id: 3,
+        question : "A set with 10 elements is considered to be:",
+        options : [
+            'Empty set',
+            'Finite set',
+            'Both A and B',
+            'None of the above',
+        ]
+    },
+    {
+        id: 4,
+        question : "A set that keeps growing and has no end is considered to be:",
+        options : [
+            'Univeral set',
+            'Infinite set',
+            'Both A and B',
+            'None of the above',
+        ]
+    },
+    {
+        id: 5,
+        question : "The set {'apple', 'banana', 'orange'}is an example of a:",
+        options : [
+            'Univeral set',
+            'Infinite set',
+            'Both A and B',
+            'None of the above',
+        ]
+    },
+
+    {
+        id: 6,
+        question : "What does 2^3 represent?",
+        options : [
+            '2 * 2 * 2',
+            '2 + 2 + 2',
+            '3*3',
+            '2/3',
+        ]
+    },
+
+    {
+        id: 7,
+        question : "How is (x^2)(x^3) simplified?",
+        options : [
+            'X^5',
+            'X^6',
+            'X^1',
+            'X^4',
+        ]
+    },
+
+    {
+        id: 8,
+        question : "Which law of indices states that am . an = am-n?",
+        options : [
+            'Zero index law',
+            'Product of powers law',
+            'Quotient of powers law',
+            'Power of a power law',
+        ]
+    },
+
+    {
+        id: 9,
+        question : "What is the value of 3^0?",
+        options : [
+            '0',
+            '3',
+            '1',
+            'None of the above',
+        ]
+    },
+
+    {
+        id: 10,
+        question : "How do we simplify (x^a)^b?",
+        options : [
+            'x^(a + b)',
+            'x^(a - b)',
+            'x^ab',
+            'x^(b/a)',
+        ]
+    },
+
+    {
+        id: 11,
+        question : "What is the product of (x + 2) and (x + 4)?",
+        options : [
+            'x^2 + 6x + 8',
+            'x^2 + 4x + 2',
+            '2x^2 + 6',
+            'x^2 + 2x',
+        ]
+    },
+
+    {
+        id: 12,
+        question : "What is the simplified form of (a + 3) (a + 2)?",
+        options : [
+            'a^2 + 3a + 2',
+            '3a^2 + 6',
+            'a^2 + 6',
+            'a^2 + 5a + 6',
+        ]
+    },
+
+    {
+        id: 13,
+        question : "What does (x - 5) (x + 3) expand to?",
+        options : [
+            'x^2 - 2x - 15',
+            'x^2 - 5x + 3',
+            'x^2 + 3x - 15',
+            'x^2 - 3x - 5',
+        ]
+    },
+
+    {
+        id: 14,
+        question : "When (m + 6) (m - 1) is simplified, what do you get?",
+        options : [
+            'm^2 - 6m + 1',
+            'm^2 + 5m - 6',
+            'm^2 + 6m - 1',
+            'm^2 - 5m + 6',
+        ]
+    },
+
+    {
+        id: 15,
+        question : "What is the result of expanding (p - 4) (p - 3)?",
+        options : [
+            'p^2 - 7p + 12',
+            'p^2 - 4p + 3',
+            'p^2 - 3p - 4',
+            'p^2 + 7p - 12',
+        ]
+    },
+];
+
+
+
+export const answers = [2,0,1,1,2,0,1,1,2,0,0,3,0,1,0];
